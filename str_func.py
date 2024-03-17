5d09a1e14c3b0927aeb3e5c09ce1fefaa33452e8
EgorPash <119717828+EgorPash@users.noreply.github.com>
2024-03-18 00:51:36 +1000

    Update str_func.py
    add new code №4

--- str_func.py
+++ str_func.py
@@ -1,4 +1,14 @@
 def capitalize_words(string):
+"""
+  Делает заглавными первые буквы каждого слова в строке.
+
+  Args:
+    string (str): Строка, в которой нужно сделать заглавными первые буквы слов.
+
+  Returns:
+    str: Строка с заглавными первыми буквами слов.
+  """
+  
   words = string.split()
 
   capitalized_words = [word.capitalize() for word in words]
